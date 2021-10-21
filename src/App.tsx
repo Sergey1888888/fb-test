e9fbadc79cf70cb7753273314a469fd716ec41c3
sergey1888888 <sergey1888888@gmail.com>
2021-10-22 01:22:29 +0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Title from './Components/Title';
+import UploadForm from './Components/UploadForm';
+import Images from './Components/Images';
+import Modal from "./Components/Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedImage, setSelectedImage] = useState<string | null>(null)
+    return (
+        <div className='App'>
+            <Title/>
+            <UploadForm/>
+            <Images setSelectedImage={setSelectedImage}/>
+            {selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage}/>}
+        </div>
+    );
 }
 
 export default App;
